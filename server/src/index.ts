561c21355ac402c23e181eb4231c23c233a80616
Christopher Villaran <villaran.christopher@gmail.com>
2021-02-18 22:20:34 +0800

    implemented redis

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,9 +3,13 @@ import { MikroORM } from '@mikro-orm/core';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
+import redis from 'redis';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
 
-// import { Post } from './entities/Post';
 import mikroConfig from './mikro-orm.config'
+import { __prod__ } from './constants';
+import { MyContext } from './types';
 import { HelloResolver } from './resolvers/hello';
 import { PostResolver } from './resolvers/post';
 import { UserResolver } from './resolvers/user';
@@ -15,12 +19,39 @@ const main = async () => {
   await orm.getMigrator().up();
 
   const app = express();
+
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  app.use(
+    session({
+      name: 'qid',
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 1 decade
+        httpOnly: true,
+        sameSite: 'lax', // protection from CSRF
+        secure: __prod__ // cookie only works in https
+      },
+      saveUninitialized: false,
+      secret: 'adfkjladlfladfdkafkljdaslfj',
+      resave: false
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false
     }),
-    context: () => ({ em: orm.em })
+    context: ({req, res}): MyContext => ({
+      em: orm.em,
+      req,
+      res
+    })
   });
 
   apolloServer.applyMiddleware({ app });
--- server/src/types.ts
+++ server/src/types.ts
@@ -1,21 +1,9 @@
 import { EntityManager, IDatabaseDriver, Connection } from '@mikro-orm/core';
-
-export type MyContext = {
-  em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>
-}
-
-/*
-
-If you get an error around 1:52:00 because of req.session.userId that's because session type was probably removed from express. So try this at     types.ts to get Session from express-session instead:
-
-import { EntityManager, IDatabaseDriver, Connection } from '@mikro-orm/core'
-import { Request, Response } from 'express'
+import { Request, Response } from 'express';
 import { Session, SessionData } from 'express-session'
 
 export type MyContext = {
-  em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>
-  req: Request & { session: Session & Partial<SessionData> & { userId?: number } }
-  res: Response
+  em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
+  req: Request & { session: Session & Partial<SessionData> & { userId?: number } };
+  res: Response;
 }
-
-*/
